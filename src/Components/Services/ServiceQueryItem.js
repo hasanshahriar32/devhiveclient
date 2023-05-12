2ea30d57f9c2c337c0482f34cc01275a1c0413aa
PRONOY <pronoybanik82@gmail.com>
2023-05-12 12:13:40 +0600

    responshiv done

--- src/Components/Services/ServiceQueryItem.js
+++ src/Components/Services/ServiceQueryItem.js
@@ -6,63 +6,52 @@ const ServiceQueryItem = ({ service }) => {
     const { _id, slugTitle, developerInfo, serviceImage, price, category } = service || {};
 
 
-    
+
     return (
         <Link to={`/services/slug/${_id}`}>
-            <div className='mx-20 my-6'>
-                <article class="flex bg-white transition hover:shadow-xl">
+            <div>
+                <div className="hero">
+                    <div className="hero-content flex-col lg:flex-row">
+                        <img src={serviceImage?.img1} className=" lg:max-w-xs rounded-lg shadow-2xl " alt='' />
+                        <div >
+                            <h1 className="text-2xl  font-bold font-serif text-gray-600 ml-8">{slugTitle}</h1>
 
-                    <div class="hidden sm:block sm:basis-56">
-                        <img
-                            alt="Guitar"
-                            src={serviceImage?.img1}
-                            class="aspect-square h-full w-full object-cover"
-                        />
-                    </div>
-                    <div class="flex flex-1 flex-col justify-between">
-                        <div class="border-s border-gray-900/10 p-4 sm:border-l-transparent sm:p-6">
-                            <a href="#">
-                                <h3 class="font-bold uppercase text-gray-900 ml-10">
-                                    {slugTitle}
-                                </h3>
-                            </a>
-
-
-                            <div className="card-body bg-state-300 ">
-                                <div className='flex'>
-                                    <div className="avatar-group -space-x-6">
-                                        <div className="avatar ">
-                                            <div className="w-12">
-                                                <img src={developerInfo?.photoURL} alt='' />
+                            <div >
+                                <div className="card-body bg-state-300 mb-6">
+                                    <div className='flex'>
+                                        <div className="avatar-group -space-x-6">
+                                            <div className="avatar ">
+                                                <div className="w-12">
+                                                    <img src={developerInfo?.photoURL} alt='' />
+                                                </div>
                                             </div>
+
                                         </div>
-
+                                        <div className='pl-4'>
+                                            <h3 className='text-left font-bold'>{developerInfo?.username}</h3>
+                                            <h2 className='font-medium'>{category?.name}</h2>
+                                        </div>
                                     </div>
-                                    <div className='pl-4'>
-                                        <h3 className='text-left font-bold'>{developerInfo?.username}</h3>
-                                        <h2 className='font-medium'>{category?.name}</h2>
+                                </div>
+
+                                <div className="card-actions justify-between items-center border-t-2 pt-4">
+                                    <div>
+                                        <div className="rating flex items-center">
+                                            <input type="radio" name="rating-1" className="mask text-sm mask-star base-400 bg-warning" />
+                                            <h2 className='text-lg pl-1'>4.8</h2>
+                                            <h3 className='text-lg pl-1'>(35)</h3>
+                                        </div>
+                                    </div>
+                                    <div className='flex'>
+                                        <h2 className='text-sm flex items-center font-medium pt-1 pr-1' >STARTING AT</h2>
+                                        <h3 className='text-2xl font-semibold'>${price}</h3>
                                     </div>
                                 </div>
                             </div>
 
-                            <div className="card-actions justify-between items-center border-t-2 pt-4">
-                                <div>
-                                    <div className="rating flex items-center">
-                                        <input type="radio" name="rating-1" className="mask text-sm mask-star base-400 bg-warning" />
-                                        <h2 className='text-lg pl-1'>4.8</h2>
-                                        <h3 className='text-lg pl-1'>(35)</h3>
-                                    </div>
-                                </div>
-                                <div className='flex'>
-                                    <h2 className='text-sm flex items-center font-medium pt-1 pr-1' >STARTING AT</h2>
-                                    <h3 className='text-2xl font-semibold'>${price}</h3>
-                                </div>
-                            </div>
                         </div>
                     </div>
-                </article>
-
-
+                </div>
 
             </div>
         </Link>
